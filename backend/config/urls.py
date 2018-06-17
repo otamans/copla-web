0a0b56d149170caa8554a6e50d7c8949785edf82
Slava <slavakyrai@gmail.com>
2018-06-17 19:47:39 +0300

    update model, urls

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -16,11 +16,6 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.conf.urls import url, include
-from django.conf import settings
-from django.conf.urls.static import static
-
-
 from django.urls import path, include
 
 urlpatterns = [
