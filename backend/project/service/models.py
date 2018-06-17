9044f5b3b21fc475ac0e070a08110c12a280792e
lemki <lwwwwwww0@gmail.com>
2018-06-17 14:56:43 +0300

    add Service & Category models

--- backend/project/service/models.py
+++ backend/project/service/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=128)
+
+
+class Service(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    photo = models.ImageField()
+    date = models.DateTimeField()
+    provide = models.BooleanField()
